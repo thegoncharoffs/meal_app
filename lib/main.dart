6d6322120af0ef0d50d37a27d8ac2afc16ad315f
Boris Goncharov <thegoncharoffs@gmail.com>
2020-11-12 17:35:05 +0300

    Added tabs and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'screens/tabs_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/meal_detail_screen.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
               ))),
       initialRoute: '/', // default value
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
